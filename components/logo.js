8dd4c4e04a122e18ef58f6e8177570ea017fa93a
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2017-09-22 16:05:33 +0530

    completely change folder structure

--- components/logo.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import styled from 'styled-components'
-import logo from './logo.png'
-
-function Header() {
-  return <img src={logo} alt="Logo" />
-}
-
-export default Header
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Button from './pages/button'
+import Button from './docs/pages/button'
 
 ReactDOM.render(<Button />, document.getElementById('root'))
